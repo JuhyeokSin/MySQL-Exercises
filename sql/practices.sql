545782dd3f0c2e8bfee0fa24cb6071ce62a03812
JuhyeokSin <juhyeok.sin@sjsu.edu>
2019-11-15 15:32:14 -0800

    added the first problem and solved it

--- sql/practices.sql
+++ sql/practices.sql
@@ -1,1 +1,10 @@
-select * from sakila.actor
+/*
+1. List how many addresses in each city
+ -  Name of the city, Number of addresses in that city
+*/
+
+SELECT c.city, COUNT(a.address) AS num_addresses
+FROM sakila.address a JOIN sakila.city c ON (a.city_id=c.city_id)
+GROUP BY c.city
+SORT BY COUNT(a.address);
+
